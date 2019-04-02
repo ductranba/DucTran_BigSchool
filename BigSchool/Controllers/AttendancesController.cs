fbc210589fbfcfdff8c315068389783e97eb6e0d
tran duc <tranbaducf5@gmail.com>
2019-04-02 21:56:19 +0700

    page 42

---
+++ BigSchool/Controllers/AttendancesController.cs
@@ -0,0 +1,37 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+
+namespace BigSchool.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseId)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CoursedId == courseId))
+                return BadRequest("The Attendance already exists!");
+            var attendance = new Attendance
+            {
+                CoursedId = courseId,
+                AttendeeId = userId
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }   
+    }
+}
--- BigSchool/Global.asax.cs
+++ BigSchool/Global.asax.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using BigSchool.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
 
 namespace BigSchool
 {
@@ -12,6 +15,8 @@ namespace BigSchool
     {
         protected void Application_Start()
         {
+            
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
